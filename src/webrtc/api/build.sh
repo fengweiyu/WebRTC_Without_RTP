29157650ceba43c27ff01dcd48521841f99b85c8
fengweiyu <984441750@qq.com>
2019-11-01 15:13:19 +0800

    [CHG]编译脚本先上传

--- src/webrtc/api/build.sh
+++ src/webrtc/api/build.sh
@@ -9,39 +9,53 @@ function PrintUsage()
 	echo -e "./build.sh arm-linux"
 }
 
+function BuildLib()
+{
+	echo -e "Start building ..."
+	OutputPath="./build"
+	if [ -e "$OutputPath" ]; then
+		if [ -e "$OutputPath/lib" ]; then
+			echo "/build/lib exit"
+		else
+			mkdir $OutputPath/lib
+		fi
+	else
+		mkdir $OutputPath
+		mkdir $OutputPath/lib
+	fi	
+	cd build
+	cmake ..
+	if [ -e "Makefile" ]; then	
+		make -j4 > /dev/null
+		if [ $? == 0 ]; then
+			echo "make success! "
+		else
+			echo "make failed! "
+			exit -1
+		fi
+	else
+		echo "Makefile generated failed! "
+		exit -1
+	fi
+	cd ..
+}
+
+function CopyLib()
+{
+	CurPwd = $PWD
+	cd $1
+	if [ -e "$OutputPath/lib" ]; then
+		echo "/build/lib exit"
+	else
+		mkdir $OutputPath/lib
+	fi
+}
 
 if [ $# == 0 ]; then
 	PrintUsage
 	exit -1
 else
 
-	cd api
-	sh build.sh $1
-	if [ $? -ne 0]; then
-		exit -1
-	fi
-	cd ..
-	
-	cd audio
-	sh build.sh $1
-	if [ $? -ne 0]; then
-		exit -1
-	fi
-	cd ..
-	
-	cd base
-	sh build.sh $1
-	if [ $? -ne 0]; then
-		exit -1
-	fi
-	cd ..
-	
-	cd call
-	sh build.sh $1
-	if [ $? -ne 0]; then
-		exit -1
-	fi
-	cd ..
 fi
 
 
