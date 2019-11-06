f49f58d91056cf885b4f3963ce8faec595537f9c
fengweiyu <984441750@qq.com>
2019-11-06 11:56:25 +0800

    [CHG]完成编译脚本，待测试。

--- src/webrtc/api/build.sh
+++ src/webrtc/api/build.sh
@@ -44,18 +44,35 @@ function CopyLib()
 {
 	CurPwd = $PWD
 	cd $1
-	if [ -e "$OutputPath/lib" ]; then
-		echo "/build/lib exit"
+	if [ -e "lib" ]; then
+		echo "lib exit"
 	else
-		mkdir $OutputPath/lib
+		mkdir lib
 	fi
+	
+	cd lib
+	if [ -e "webrtc" ]; then
+		echo "webrtc exit"
+	else
+		mkdir webrtc
+	fi
+	
+	cd webrtc
+	if [ -e "api" ]; then
+		echo "api exit"
+	else
+		mkdir api
+	fi
+	cp $CurPwd/build/lib/libapi.a .
+	
 }
 
 if [ $# == 0 ]; then
 	PrintUsage
 	exit -1
 else
-
+	BuildLib
+	CopyLib ../../../build
 fi
 
 
