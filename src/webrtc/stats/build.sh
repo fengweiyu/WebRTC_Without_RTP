8f5f183dba10fe6752b11bcaaacb5840ada770c8
fengweiyu <984441750@qq.com>
2020-01-02 11:52:58 +0800

    [CHG]更新编译脚本

---
+++ src/webrtc/stats/build.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+function PrintUsage()
+{
+	echo -e "Usage:"
+	echo -e "./build.sh $ToolChain"
+	echo -e "ToolChain: arm-linux"
+	echo -e "EGG:"
+	echo -e "./build.sh arm-linux"
+}
+function GenerateCmakeFile()
+{
+	CmakeFile="./build/ToolChain.cmake"
+	echo "SET(CMAKE_SYSTEM_NAME \"Linux\")" > $CmakeFile
+	echo "SET(CMAKE_C_COMPILER \"$1-gcc\")" >> $CmakeFile
+	echo "SET(CMAKE_CXX_COMPILER \"$1-g++\")" >> $CmakeFile
+}
+function BuildLib()
+{
+	echo -e "Start building ..."
+	OutputPath="./build"
+	if [ -e "$OutputPath" ]; then
+		if [ -e "$OutputPath/lib" ]; then
+			echo "/build/lib exit"
+		else
+			mkdir $OutputPath/lib
+		fi
+	else
+		mkdir $OutputPath
+		mkdir $OutputPath/lib
+	fi	
+	cd build
+	cmake ..
+	if [ -e "Makefile" ]; then	
+		make -j4 > /dev/null
+		if [ $? == 0 ]; then
+			echo "make success! "
+		else
+			echo "make failed! "
+			exit -1
+		fi
+	else
+		echo "Makefile generated failed! "
+		exit -1
+	fi
+	cd ..
+}
+
+function CopyLib()
+{
+	CurPwd = $PWD
+	cd $1
+	if [ -e "lib" ]; then
+		echo "lib exit"
+	else
+		mkdir lib
+	fi
+	
+	cd lib
+	if [ -e "webrtc" ]; then
+		echo "webrtc exit"
+	else
+		mkdir webrtc
+	fi
+	
+	cd webrtc
+	if [ -e "stats" ]; then
+		echo "stats exit"
+	else
+		mkdir stats
+	fi
+	cp $CurPwd/build/lib/librtc_stats.a .
+	
+}
+
+if [ $# == 0 ]; then
+	PrintUsage
+	exit -1
+else
+	GenerateCmakeFile $1	
+	BuildLib
+	CopyLib ../../../build
+fi
+
+
+
+
